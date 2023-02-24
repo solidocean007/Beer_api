df3955d9aa92f02b7a54c83c6c4b1b99ca0f4be4
solidocean007 <clintonwilliams007@gmail.com>
2023-02-23 20:18:19 -0500

    update

--- script/api.js
+++ script/api.js
@@ -26,3 +26,72 @@
 // getAllBreweries().then((breweries) => {
 //   console.log(breweries);
 // });
+
+// .catch((error) => console.log(error));
+
+// function buildBreweryList(arr) {
+//   for (let i = 0; i < breweryArray.length; i++) {
+//     const brewBox = document.createElement("div");
+//     brewBox.classList.add("brewBox");
+//     showcase.append(brewBox);
+
+//     const iconBox = document.createElement("div");
+//     iconBox.classList.add("iconBox");
+//     brewBox.append(iconBox);
+
+// const favIcon = document.createElement('i');
+// favIcon.className ="fa-solid fa-beer-mug-empty";
+// iconBox.append(favIcon);
+
+// const beerTab = document.createElement("img");
+// beerTab.setAttribute("data-select", "");
+// beerTab.className = "beerTab";
+// beerTab.setAttribute("title", "Favorite");
+// beerTab.src = "/images/beer_tab.png";
+// iconBox.append(beerTab);
+
+// const brewInfo = document.createElement("div");
+// brewInfo.classList.add("brewInfo");
+// brewBox.append(brewInfo);
+
+// const brewName = document.createElement("h3");
+// brewName.textContent = arr[i].name;
+// brewInfo.append(brewName);
+
+// const cityDiv = document.createElement("div");
+// cityDiv.classList.add("cityDiv");
+// const cityText = document.createElement("h5");
+// cityText.textContent = `${arr[i].city}, ${arr[i].state}`;
+// brewBox.append(cityDiv);
+// cityDiv.append(cityText);
+
+// const webAddress = document.createElement("div");
+// webAddress.classList.add("webAddress");
+// const webText = document.createElement("a");
+
+// if (arr[i].website_url) {
+//   webText.href = arr[i].website_url;
+//   webText.textContent = 'Click to visit website';
+// } else { webText.textContent = 'No website available';}
+// brewBox.append(webAddress);
+// webAddress.append(webText);
+// console.log(`breweryArray.[i]`);
+
+// const iconBox = document.createElement("div");
+// iconBox.classList.add("iconBox");
+// brewBox.append(iconBox);
+
+// const beerTab = document.createElement("img");
+// beerTab.setAttribute("data-select", "");
+// beerTab.className = "beerTab";
+// beerTab.setAttribute("title", "Favorite");
+// beerTab.src = "/images/beer_tab.png";
+// iconBox.append(beerTab);
+
+// const brewInfo = document.createElement("div");
+// brewInfo.classList.add("brewInfo");
+// brewBox.append(brewInfo);
+
+// const brewName = document.createElement("h3");
+// brewName.textContent = item.name;
+// brewInfo.append(brewName);
--- script/main.js
+++ script/main.js
@@ -1,38 +1,74 @@
 const showcase = document.querySelector("section");
-
+let breweryArray = [];
+let favoriteArray = [];
 const apiUrlNC =
   "https://api.openbrewerydb.org/breweries?by_state=north_carolina&per_page=30";
+
+const invisible = "invisible";
+
 fetch(apiUrlNC)
   .then((response) => response.json())
-  .then((responseJSon) => {
-    console.log(responseJSon);
-    for (let i = 0; i < responseJSon.length; i++) {
+  .then((data) => {
+    data.forEach((item) => {
       const brewBox = document.createElement("div");
       brewBox.classList.add("brewBox");
+      brewBox.innerHTML = `
+      <div class = 'iconBox'>
+        <img data-select="" class="beerTab" title="Favorite" 
+        src="/images/beer_tab.png">
+      </div>
+      <div class = 'brewInfo'>
+        <h3>${item.name}</h3>
+      </div>
+      `;
       showcase.append(brewBox);
 
-      const iconBox = document.createElement('div');
-      iconBox.classList.add('iconBox');
-      brewBox.append(iconBox);
-      
-      const favIcon = document.createElement('i');
-      favIcon.className ="fa-solid fa-beer-mug-empty";
-      iconBox.append(favIcon);
-
-
-      const brewInfo = document.createElement("div");
-      brewInfo.classList.add('brewInfo');
-      brewBox.append(brewInfo);
-
-      const brewName = document.createElement('h3');
-      brewName.textContent = responseJSon[i].name;
-      brewInfo.append(brewName);
-
-      const cityDiv = document.createElement('div');
-      cityDiv.classList.add('cityDiv');
-      const cityText = document.createElement('h5');
-      cityText.textContent = responseJSon[i].city;
+      const cityDiv = document.createElement("div");
+      cityDiv.classList.add("cityDiv");
+      const cityText = document.createElement("h5");
+      cityText.textContent = `${item.city}, ${item.state}`;
       brewBox.append(cityDiv);
       cityDiv.append(cityText);
-    }
+
+      const webAddress = document.createElement("div");
+      webAddress.classList.add("webAddress");
+      const webText = document.createElement("a");
+
+      if (item.website_url) {
+        webText.href = item.website_url;
+        webText.textContent = "Click to visit website";
+      } else {
+        webText.textContent = "No website available";
+      }
+      brewBox.append(webAddress);
+      webAddress.append(webText);
+    });
   });
+  
+showcase.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.classList.contains("beerTab")) {
+    const div = target.parentElement.parentElement;
+    div.classList.add(invisible);
+
+    const ul = document.getElementById('favorites-list');
+    const name = div.querySelector('.brewInfo').textContent;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <i title = "remove" class="fa-solid fa-bookmark"></i>
+    ${name}
+    `
+    ul.appendChild(listItem);
+
+    // favoriteArray.push(div);
+  }
+  if (target.classList.contains('fa-bookmark')) {
+    console.log(target);
+    // const li = target.parentElement;
+    // console.log(li);
+    // const div = li.previousElementSibling;
+    // li.remove();
+    // div.classList.remove(invisible);
+  }
+});
+
